91a8403b5420d62b970f5374b0a121d306da0396
listar2000 <lisd.star2015@outlook.com>
2021-03-24 13:19:03 -0700

    add download script

---
+++ download.sh
@@ -0,0 +1,8 @@
+mkdir data
+cd data
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_validate_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_train_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_test_filter.lst
+wget http://lstm.seas.harvard.edu/latex/data/formula_images_processed.tar.gz
+wget http://lstm.seas.harvard.edu/latex/data/im2latex_formulas.norm.lst
+tar -zxvf formula_images_processed.tar.gz
